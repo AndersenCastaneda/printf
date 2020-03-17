c3786f376da3375f028ef05e9ed0e26d3caa5f81
AndersenCastaneda <andersenpiano@gmail.com>
2020-03-17 21:13:46 +0000

    Fix code

--- _printf.c
+++ _printf.c
@@ -54,50 +54,43 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format[i]; i++)
 	{
-		/*if the char is not NULL and is diferent of '%'*/
-		/*Basically any char*/
-		while (format[i] && format[i] != '%')
-		{
-			/*Print and count the character*/
-			_write(format[i]);
-			len++;
-			i++;
-		}
-
-		/*if i got to the end of the format, return the legth*/
-		if (format[i] == '\0')
-			return (len);
-
-		/*if char is equal to '%'*/
 		if (format[i] == '%')
 		{
-			/*Call my handler to check the next char*/
-			f = format_handler(format[i + 1]);
+			i++;
+			while (format[i] && format[i] == ' ')
+				i++;
 
-			/*if there is a coincidence, i get the  */
-			/*correct function to call              */
+			if(!format[i])
+				return (-1);
+
+			/*Call my handler to check the char*/
+			f = format_handler(format[i]);
+
 			if (f)
 			{
 				/*get the length of the variable to print*/
 				len += f(al);
 
-				/*move two steps becuase % is first step*/
-				/*and de char is the second step        */
-				/*i++ and i++ on the for sentence       */
-				i++;
-				continue;
+			}
+			else
+			{
+				len += _print_perc(al);
+				len += _write(format[i]);
 			}
 		}
-
-		/*if the char is equal to '%' and there is not coincidence*/
+		else
+		{
+			len += _write(format[i]);
+		}
+/*
+		if the char is equal to '%' and there is not coincidence
 		if (format[i] == '%' && !f)
 		{
-			/*Call print percentage*/
+			Call print percentage
 			len += _print_perc(al);
 		}
-
+*/
 	}
-
 	/*frees the argument list*/
 	va_end(al);
 	return (len);
--- test/main.c
+++ test/main.c
@@ -13,7 +13,8 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-
+	_printf("%     s\n", "Holi");
+	printf("%     s\n", "Holi");
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
